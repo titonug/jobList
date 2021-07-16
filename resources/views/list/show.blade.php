1d75e3d17dd2c9f45348e3a3e895b7bf57f394dc
titonug <tito.nugroho666@gmail.com>
2021-07-16 17:56:54 +0700

    Menambah dan memperbaiki tampilan

--- resources/views/list/show.blade.php
+++ resources/views/list/show.blade.php
@@ -11,16 +11,19 @@
     <dt class="col-sm-3"></dt>
     <dd class="col-sm-3">{{ $data['title'] }}</dd>
 
-    <dd class="col-sm-9">{!! $data['description'] !!}</dd>
-    
-    <dt class="col-sm-3">How to Apply</dt>
-    <dt class="col-sm-3">Send Your Resume Or Visit Here</dt>
-    <dd class="col-sm-9">{!! $data['how_to_apply'] !!}</dd>
-
     <dd class="col-sm-9">
         <img width="500px" 
         src="@php echo $data['company_logo'] @endphp">
     </dd>
+    <dd class="col-sm-9"><a href="{{ $data['company_url'] }}">{{ $data['company_url'] }}</a></dd>
+    <dt class="col-sm-9">How to Apply</dt>
+    <dd class="col-sm-9">{!! $data['how_to_apply'] !!}</dd>
+
+    <dd class="col-sm-9">{!! $data['description'] !!}</dd>
+    
+    
+    
+    
 
 </dl>
 
